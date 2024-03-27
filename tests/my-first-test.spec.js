01785f763508a2d3ca6699105caa06fbbb1d4e45
Patrick Eakin <patrickeakin@Patricks-Mac-Studio.local>
2024-03-27 16:33:05 -0400

    remove pause

--- tests/my-first-test.spec.js
+++ tests/my-first-test.spec.js
@@ -5,9 +5,8 @@ test.beforeEach('go to todo app', async({ page }) => {
     await page.goto('/todomvc')
 })
 
-test.only('Can create todo item', async({ page }) => {
+test.only('Can create todo items', async({ page }) => {
     const textInput = new TextInput(page)
-    await page.pause()
     const toDoItem = await textInput.createToDoItem('my first todo')
     await expect(toDoItem.itemLabel).toBeVisible()
 })
