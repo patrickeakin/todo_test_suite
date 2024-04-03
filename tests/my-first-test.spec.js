05b8ff080c3279d74eff068abc0847c7972d1eda
Patrick Eakin <patrickeakin@Patricks-Mac-Studio.local>
2024-04-03 15:30:31 -0400

    add test to check input is empty

--- tests/my-first-test.spec.js
+++ tests/my-first-test.spec.js
@@ -6,12 +6,19 @@ test.beforeEach('go to todo app', async({ page }) => {
     await page.goto('/todomvc')
 })
 
-test.only('Can create todo items', async({ page }) => {
-    const itemTitles = [ '1st item', '2nd item' ]
+const itemTitles = [ '1st item', '2nd item' ]
+
+test('Can create todo items', async({ page }) => {
     const textInput = new TextInput(page)
     await textInput.createToDoItem(itemTitles[0])
     await textInput.createToDoItem(itemTitles[1])
 
     const toDoItem = new ToDoItem(page)
     await expect(toDoItem.itemLabel).toHaveText(itemTitles)
+})
+
+test.only('Text field is clear after adding item', async({ page }) => {
+    const textInput = new TextInput(page)
+    await textInput.createToDoItem(itemTitles[0])
+    await expect(textInput.input).toBeEmpty()
 })
