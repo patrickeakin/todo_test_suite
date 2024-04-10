b280438a4d5c3cd0f467dadbcaac1402af5fc6a6
Patrick Eakin <patrickeakin@Patricks-Mac-Studio.local>
2024-04-10 11:37:59 -0400

    add test new items go to bottom of list

--- tests/my-first-test.spec.js
+++ tests/my-first-test.spec.js
@@ -7,18 +7,27 @@ test.beforeEach('go to todo app', async({ page }) => {
 })
 
 const itemTitles = [ '1st item', '2nd item' ]
-
-test('Can create todo items', async({ page }) => {
+const createToDos = async (page) => {
     const textInput = new TextInput(page)
     await textInput.createToDoItem(itemTitles[0])
     await textInput.createToDoItem(itemTitles[1])
+    return textInput
+}
+
+test('Can create todo items', async({ page }) => {
+    const textInput = await createToDos(page)
 
     const toDoItem = new ToDoItem(page)
     await expect(toDoItem.itemLabel).toHaveText(itemTitles)
 })
 
-test.only('Text field is clear after adding item', async({ page }) => {
-    const textInput = new TextInput(page)
-    await textInput.createToDoItem(itemTitles[0])
+test('Text field is clear after adding item', async({ page }) => {
+    const textInput = await createToDos(page)
     await expect(textInput.input).toBeEmpty()
+})
+
+test('New items go to the bottom of the list', async({ page }) => {
+    await createToDos(page)
+    const lastToDoText = await page.getByTestId('todo-title').last().innerText()
+    await expect(lastToDoText).toBe(itemTitles[1])
 })
